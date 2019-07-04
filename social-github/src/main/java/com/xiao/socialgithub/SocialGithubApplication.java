764cc76e1fecd653761ab4e1f14616390f3594d5
Jinwei Sun <>
2019-07-04 22:40:21 +0800

    添加github连接

---
+++ social-github/src/main/java/com/xiao/socialgithub/SocialGithubApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.socialgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialGithubApplication.class, args);
+    }
+
+}
---
+++ social-github/src/main/java/com/xiao/socialgithub/config/GitHubProperties.java
@@ -0,0 +1,8 @@
+package com.xiao.socialgithub.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.social.github")
+public class GitHubProperties extends SocialProperties {
+
+}
